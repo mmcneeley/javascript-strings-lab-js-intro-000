e584c564647d3f53efa0f1773f01484bdc28717b
Mike McNeeley <mmcneeley@gmail.com>
2018-02-05 22:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "hello everybody!";
+var greeting = "hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "hello" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
